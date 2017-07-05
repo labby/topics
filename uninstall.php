d6189d641a70f580d13600aad36fa82951224efd
labby <rp@cms-lab.com>
2017-07-05 12:38:11 +0200

    add sik tables

--- uninstall.php
+++ uninstall.php
@@ -21,13 +21,17 @@ $tablename = $mod_dir;
 include(LEPTON_PATH.'/modules/'.$mod_dir.'/defaults/module_settings.default.php');
 if (file_exists(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php')) { include(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php'); }
 
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."mod_".$tablename."_obsolete`");
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."mod_".$tablename."_comments_obsolete`");
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."mod_".$tablename."_settings_obsolete`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename"`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_comments`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_settings`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_rss_count`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_statistic`");
 
-$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."` TO `".TABLE_PREFIX."mod_".$tablename."_obsolete`");
-$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_comments` TO `".TABLE_PREFIX."mod_".$tablename."_comments_obsolete`");
-$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_settings` TO `".TABLE_PREFIX."mod_".$tablename."_settings_obsolete`");
+$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."` TO `".TABLE_PREFIX."xsik_".$tablename."`");
+$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_comments` TO `".TABLE_PREFIX."xsik_".$tablename."_comments`");
+$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_settings` TO `".TABLE_PREFIX."xsik_".$tablename."_settings`");
+$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_rss_count` TO `".TABLE_PREFIX."xsik_".$tablename."_rss_count`");
+$database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_rss_statistic` TO `".TABLE_PREFIX."xsik_".$tablename."_rss_statistic`");
 
 require_once(LEPTON_PATH.'/framework/summary.functions.php');
 rm_full_dir(LEPTON_PATH.$topics_directory);
