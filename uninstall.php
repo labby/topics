89197f29167570d68d27a3473ab17112e1e65b25
labby <rp@cms-lab.com>
2017-07-05 13:24:37 +0200

    bugfix

--- uninstall.php
+++ uninstall.php
@@ -19,9 +19,11 @@ $mod_dir = basename(dirname(__FILE__));
 $tablename = $mod_dir;
 // include module_settings
 include(LEPTON_PATH.'/modules/'.$mod_dir.'/defaults/module_settings.default.php');
-if (file_exists(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php')) { include(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php'); }
+if (file_exists(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php')) { 
+include(LEPTON_PATH.'/modules/'.$mod_dir.'/module_settings.php'); 
+}
 
-$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename"`");
+$database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."`");
 $database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_comments`");
 $database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_settings`");
 $database->query("DROP TABLE IF EXISTS `".TABLE_PREFIX."xsik_".$tablename."_rss_count`");
@@ -35,5 +37,7 @@ $database->query("RENAME TABLE `".TABLE_PREFIX."mod_".$tablename."_rss_statistic
 
 require_once(LEPTON_PATH.'/framework/summary.functions.php');
 rm_full_dir(LEPTON_PATH.$topics_directory);
+
+
 
 ?>
